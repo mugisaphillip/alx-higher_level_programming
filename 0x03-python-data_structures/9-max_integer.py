a9a4dec3a7d865e0ee89d2e1e554e8f4e31b8acb
phillip mugisawq <phillipmugisa@icloud.com>
2023-06-12 16:47:13 +0300

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_value = my_list[0]
+    if isinstance(my_list, list):
+        if len(my_list) == 0:
+            return None
+        for i in my_list:
+            if i > max_value:
+                max_value = i
+        return max_value
