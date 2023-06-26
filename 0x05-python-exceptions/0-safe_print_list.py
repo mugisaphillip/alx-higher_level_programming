6afd3a456c3664f61a36cf29ef95b4a850dd0862
phillip mugisawq <phillipmugisa@icloud.com>
2023-06-26 22:24:01 +0300

    python exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for p in range(x):
+        try:
+            print("{}".format(my_list[p]), end="")
+            i += 1
+        except IndexError:
+            break
+    print("")
+    return (i)
