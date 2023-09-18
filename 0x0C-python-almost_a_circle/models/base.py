93d3644fd41fd1c576b8de1ec53f96f9738f1510
phillip mugisawq <phillipmugisa@icloud.com>
2023-09-18 19:41:21 +0300

    creating base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-"""
-Base - base class
-"""
+"Defines a base model class"
 
 
 class Base:
+    """
+        Represent Base class model
+        Attributes:
+            __nb_objects (int)L The number is instances without id
+    """
+
+
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        initializing objects
+        Args:
+            id (int): id of new object
+        """
         if id is not None:
             self.id = id
         else:
